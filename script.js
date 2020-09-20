a052817572825bc586f0a56a0829ea82757a2472
dimaOwens <dimaOwens>
2020-09-20 21:37:10 +0300

    updated

--- script.js
+++ script.js
@@ -2,49 +2,46 @@ var users=[];
 $("#create-acc").click(function(){
 	window.open("index2.html");
 })
+var sub = document.getElementById("regis");
+sub.addEventListener('click',function(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	var userName = $("#user-name").val();
+	var userBalance = $("#balance").val();
+	var pass = $("#password").val();
+	var id = creatAcc(userName,userBalance,pass);
+	$("#id-note").empty()
+	var boldId = $("<b></b>").text(id)
+	var container = $("<div></div>");
+	$(container).text("Your id is: ")
+	$(container).attr("id","note")
+    $(container).append(boldId);
+    $("#id-note").append(container)
+})
+$("#login").click(function(){
+	var userId = $("#ui").val();
+	var pw = $("#ps").val();
+	$.each(users, function(i, usr){
+		if(usr['id'] === userId && usr['password'] === pw){
+			window.open("index3.html");
+		}
+	})
+})
 
 function creatAcc(username,balance,password){
 var account={
 username:username,
 balance:balance,
 password:password,
-id: generateId();
+id: generateId(),
 transaction:transaction
 };
 users.push(account)
-
+return account.id;
 
 }
 
 function generateId(){
-  this.id = Math.floor(Math.random() * (100000))	
+ return Math.floor(Math.random() * (100000))	
 }
 function transaction(){
 var s = this
