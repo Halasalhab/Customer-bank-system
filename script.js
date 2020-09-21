663192d1907fc0286f9feb00258307efddced8ce
dimaOwens <dimaOwens>
2020-09-21 11:57:55 +0300

    Done

--- script.js
+++ script.js
@@ -1,45 +1,93 @@
 var users=[];
+var usr1 = creatAcc("Hala", 5000, "98754");
+var usr2 = creatAcc("laila", 8000 , "lkjh");
+var transferedUserIndex;
+users = [usr1, usr2];
+$("#index2").hide();
+$("#index3").hide();
 $("#create-acc").click(function(){
-	window.open("index2.html");
+	$("#index").hide();
+	$("#index2").show();
 })
 
 $("#regis").click(function(){
 
 	var userName = $("#user-name").val();
 	var userBalance = $("#balance").val();
+	userBalance = parseInt(userBalance)
+
 	var pass = $("#password").val();
-	var id = creatAcc(userName,userBalance,pass);
+	if(userBalance < 0){
+		alert("Your balance can't be less than 0")
+		return;
+	}
+	var usr = creatAcc(userName,userBalance,pass);
+	var id = usr["id"]
 	$("#id-note").empty()
 	var boldId = $("<b></b>").text(id)
 	var container = $("<div></div>");
 	$(container).text("Your id is: ")
 	$(container).attr("id","note")
     $(container).append(boldId);
+    alert("You registered successfully, Thank you for choosing us")
     $("#id-note").append(container)
+    $("#index2").hide();
+    $("#index").show();
+
+
 })
 $("#login").click(function(){
 	var userId = $("#ui").val();
 	var pw = $("#ps").val();
-	$.each(users, function(i, usr){
-		if(usr['id'] === userId && usr['password'] === pw){
-			window.open("index3.html");
+	$.each(users, function(i, person){
+		if(person['id'].toString() === userId && person['password'] === pw){
+            transferedUserIndex = i;
+            $("#index").hide()
+            $("#index3").show()
+            return;
 		}
 	})
 })
 
-/*$("#balance-li").click(function(){
+ $("#balance-li").one('click',function(){
+ 	 var balance = users[transferedUserIndex]['balance'];
 
-})*/
+ 	 $("#balance-li").append(balance)
+})
+
+$("#depos").click(function(){
+    var amoun = parseInt($("#amount").val())
+	users[transferedUserIndex].transaction().deposit(amoun)
+    alert("Process done successfully balance now is: " + users[transferedUserIndex]['balance'])
+	
+})
+
+$("#withdraw").click(function(){
+	var amoun = parseInt($("#amount").val())
+	orgBalance= users[transferedUserIndex]['balance']
+	users[transferedUserIndex].transaction().withdrow(amoun)
+	if(users[transferedUserIndex]['balance'] === orgBalance){
+		return;	
+	}
+    alert("Process done successfully balance now is: " + users[transferedUserIndex]['balance'])
+
+})
+
+$("#the-richeset").one('click',function(){
+	var richest = maximumAccountBalance();
+
+	$("#the-richeset").append(richest['balance'])
+})
 function creatAcc(username,balance,password){
 var account={
 username:username,
-balance:balance,
+balance:parseInt(balance),
 password:password,
 id: generateId(),
 transaction:transaction
 };
 users.push(account)
-return account.id;
+return account;
 
 }
 
@@ -51,7 +99,6 @@ var s = this
 var balance = this.balance;
 return {
 	deposit:function(amount){
-		console.log(balance)
 		s.balance = balance+amount;
 	},
 	withdrow:function(amount){
